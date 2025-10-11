d2b1c6efa6b22161c8343eb1e1439f0c5520b37c
lerodic <daniel@rodev.net>
2025-10-11 14:56:22 +0200

    test(app): Add unit tests for BatoCrawler

---
+++ tests/BatoCrawler.test.ts
@@ -0,0 +1,51 @@
+import "reflect-metadata";
+import BatoCrawler from "../src/core/crawl/crawlers/BatoCrawler";
+import Chromium from "../src/core/crawl/Chromium";
+import { Page } from "puppeteer";
+
+describe("BatoCrawler", () => {
+  let crawler: BatoCrawler;
+  let mockPage: jest.Mocked<Page>;
+  let mockChromium: jest.Mocked<Chromium>;
+
+  beforeEach(() => {
+    mockPage = {
+      $eval: jest.fn(),
+      close: jest.fn(),
+    } as unknown as jest.Mocked<Page>;
+
+    mockChromium = {
+      openPage: jest.fn().mockReturnValue(mockPage),
+    } as unknown as jest.Mocked<Chromium>;
+
+    crawler = new BatoCrawler(mockChromium);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("extractTitle", () => {
+    it.each([
+      {
+        url: "https://example.com",
+        title: "First Manga",
+      },
+      {
+        url: "https://another-example.com",
+        title: "Second Manga",
+      },
+    ])(
+      "should return title: '$title' for URL: '$url'",
+      async ({ url, title }) => {
+        mockPage.$eval.mockImplementationOnce((_selector: string, fn: any) => {
+          return fn({ textContent: title });
+        });
+
+        const result = await crawler.extractTitle(url);
+
+        expect(result).toStrictEqual(title);
+      }
+    );
+  });
+});
