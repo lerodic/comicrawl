5237a230d34f5bf0bc994b6fc749627b8ac95879
lerodic <daniel@rodev.net>
2025-10-11 14:42:42 +0200

    test(app): Add unit tests for "Chromium"

---
+++ tests/Chromium.test.ts
@@ -0,0 +1,122 @@
+import "reflect-metadata";
+import Chromium from "../src/core/crawl/Chromium";
+import MissingChromiumInstance from "../src/core/errors/MissingChromiumInstance";
+import CONFIG from "../src/config/app.config";
+import puppeteer, { Browser, Page } from "puppeteer";
+
+jest.mock("puppeteer");
+jest.mock("../src/config/app.config", () => ({
+  __esModule: true,
+  default: {
+    SUPPORTED_DOMAINS: ["https://mocked.com", "https://mocked2.com"],
+    EXECUTABLE_PATH: "/mocked/path",
+  },
+}));
+
+describe("Chromium", () => {
+  let chromium: Chromium;
+  let mockPuppeteer = puppeteer as jest.Mocked<typeof puppeteer>;
+  let mockBrowser: jest.Mocked<Browser>;
+  let mockPage: jest.Mocked<Page>;
+
+  beforeEach(() => {
+    mockPage = {
+      setDefaultTimeout: jest.fn(),
+      setDefaultNavigationTimeout: jest.fn(),
+      goto: jest.fn(),
+    } as unknown as jest.Mocked<Page>;
+
+    mockBrowser = {
+      newPage: jest.fn().mockResolvedValue(mockPage),
+      close: jest.fn(),
+    } as unknown as jest.Mocked<Browser>;
+
+    chromium = new Chromium();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+
+    CONFIG.SUPPORTED_DOMAINS = [
+      "https://mocked.com",
+      "https://another-mocked.com",
+    ];
+    CONFIG.EXECUTABLE_PATH = "/mocked/path";
+  });
+
+  describe("openPage", () => {
+    it("should set up new page correctly", async () => {
+      CONFIG.EXECUTABLE_PATH = undefined;
+      mockPuppeteer.launch.mockResolvedValue(mockBrowser);
+
+      await chromium.openPage("https://somepage.com");
+
+      expect(mockPage.setDefaultTimeout).toHaveBeenCalledWith(0);
+      expect(mockPage.setDefaultNavigationTimeout).toHaveBeenCalledWith(0);
+    });
+
+    it.each(["https://example.com", "https://another-one.com"])(
+      "should navigate to %s using bundled-in chromium instance",
+      async (url) => {
+        CONFIG.EXECUTABLE_PATH = undefined;
+        mockPuppeteer.launch.mockResolvedValue(mockBrowser);
+
+        const result = await chromium.openPage(url);
+
+        expect(mockPuppeteer.launch).not.toHaveBeenCalledWith({
+          executablePath: CONFIG.EXECUTABLE_PATH,
+        });
+        expect(result).toStrictEqual(mockPage);
+      }
+    );
+
+    it.each(["https://example.com", "https://another-one.com"])(
+      "should navigate to %s using user-defined chromium instance",
+      async (url) => {
+        mockPuppeteer.launch.mockResolvedValue(mockBrowser);
+
+        const result = await chromium.openPage(url);
+
+        expect(mockPuppeteer.launch).toHaveBeenCalledWith({
+          executablePath: CONFIG.EXECUTABLE_PATH,
+        });
+        expect(result).toStrictEqual(mockPage);
+      }
+    );
+
+    it.each([
+      {
+        url: "https://example.com",
+        path: "some/random/path",
+      },
+      {
+        url: "https://another-one.com",
+        path: "some/other/path",
+      },
+    ])(
+      "should throw 'MissingChromiumInstance' error if user-defined instance could not be found",
+      async ({ url, path }) => {
+        mockPuppeteer.launch.mockImplementation(() => {
+          throw new MissingChromiumInstance(path);
+        });
+
+        await expect(chromium.openPage(url)).rejects.toThrow(
+          MissingChromiumInstance
+        );
+      }
+    );
+  });
+
+  describe("terminate", () => {
+    it("should correctly terminate any running Chromium instances", async () => {
+      mockPuppeteer.launch.mockResolvedValue(mockBrowser);
+      await chromium.openPage("");
+
+      await chromium.terminate();
+
+      expect(mockBrowser.close).toHaveBeenCalledTimes(1);
+      expect((mockBrowser as any).page).toBeUndefined();
+      expect((mockBrowser as any).browser).toBeUndefined();
+    });
+  });
+});
