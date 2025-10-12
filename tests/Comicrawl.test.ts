d4e24c6ff573978b8b0fd9fc96d77c1b9e5804ce
lerodic <daniel@rodev.net>
2025-10-12 20:36:17 +0200

    test(app): Update unit tests for "Comicrawl"

--- tests/Comicrawl.test.ts
+++ tests/Comicrawl.test.ts
@@ -3,17 +3,23 @@ import Comicrawl from "../src/core/Comicrawl";
 import Prompt from "../src/core/io/Prompt";
 import { Crawler } from "../src/types";
 import CrawlerFactory from "../src/core/factories/CrawlerFactory";
-import Chromium from "../src/core/crawl/Chromium";
 import Logger from "../src/core/io/Logger";
+import ProgressManager from "../src/core/io/ProgressManager";
 import CrawlerInitializationFailed from "../src/core/errors/CrawlerInitializationFailed";
+
+jest.mock("p-limit", () => {
+  return () => {
+    return (fn: (...args: any[]) => any) => fn();
+  };
+});
 
 describe("Comicrawl", () => {
   let comicrawl: Comicrawl;
   let mockPrompt: jest.Mocked<Prompt>;
   let mockCrawler: jest.Mocked<Crawler>;
   let mockCrawlerFactory: jest.Mocked<CrawlerFactory>;
-  let mockChromium: jest.Mocked<Chromium>;
   let mockLogger: jest.Mocked<Logger>;
+  let mockProgressManager: jest.Mocked<ProgressManager>;
 
   beforeEach(() => {
     mockPrompt = {
@@ -26,28 +32,33 @@ describe("Comicrawl", () => {
     mockCrawler = {
       extractTitle: jest.fn(),
       extractChapters: jest.fn(),
+      extractImageLinks: jest.fn(),
+      terminate: jest.fn(),
     } as unknown as jest.Mocked<Crawler>;
 
     mockCrawlerFactory = {
       getCrawler: jest.fn().mockReturnValue(mockCrawler),
     } as unknown as jest.Mocked<CrawlerFactory>;
 
-    mockChromium = {
-      terminate: jest.fn(),
-    } as unknown as jest.Mocked<Chromium>;
-
     mockLogger = {
       info: jest.fn(),
       error: jest.fn(),
       logChapterRequest: jest.fn(),
       logChaptersFound: jest.fn(),
+      logDownloadStarted: jest.fn(),
     } as unknown as jest.Mocked<Logger>;
+
+    mockProgressManager = {
+      createChapterPreparationBar: jest.fn(),
+      advanceChapterPreparation: jest.fn(),
+      completeChapterPreparation: jest.fn(),
+    } as unknown as jest.Mocked<ProgressManager>;
 
     comicrawl = new Comicrawl(
       mockPrompt,
       mockCrawlerFactory,
-      mockChromium,
-      mockLogger
+      mockLogger,
+      mockProgressManager
     );
   });
 
@@ -56,45 +67,39 @@ describe("Comicrawl", () => {
   });
 
   describe("run", () => {
-    it.each([
-      {
-        url: "https://example.com",
-        title: "Comic 1",
-        chapters: [
-          {
-            title: "Chapter 1",
-            url: "example.com/chapter-1",
-          },
-        ],
-      },
-      {
-        url: "https://test.com",
-        title: "Manga 1",
-        chapters: [
-          {
-            title: "First chapter",
-            url: "test.com/first-chapter",
-          },
-        ],
-      },
-    ])(
-      "should extract all chapters successfully",
-      async ({ url, title, chapters }) => {
-        const logSpy = jest.spyOn(console, "log");
-        mockPrompt.getUrl.mockResolvedValue(url);
-        mockPrompt.getDownloadOption.mockResolvedValue("All");
-        mockCrawler.extractTitle.mockResolvedValue(title);
-        mockCrawler.extractChapters.mockResolvedValue(chapters);
+    it("should extract all chapters successfully", async () => {
+      const url = "https://example.com";
+      const title = "Comic 1";
+      const chapters = [
+        {
+          title: "Chapter 1",
+          url: "example.com/chapter-1",
+        },
+      ];
+      const imageLinks = ["img1", "img2", "img3"];
 
-        await comicrawl.run();
+      mockPrompt.getUrl.mockResolvedValue(url);
+      mockPrompt.getDownloadOption.mockResolvedValue("All");
+      mockCrawler.extractTitle.mockResolvedValue(title);
+      mockCrawler.extractChapters.mockResolvedValue(chapters);
+      mockCrawler.extractImageLinks.mockResolvedValue(imageLinks);
 
-        expect(mockCrawler.extractTitle).toHaveBeenCalled();
-        expect(mockCrawler.extractChapters).toHaveBeenCalled();
-        expect(logSpy).toHaveBeenNthCalledWith(1, title);
-        expect(logSpy).toHaveBeenNthCalledWith(2, chapters);
-        expect(mockChromium.terminate).toHaveBeenCalledTimes(1);
-      }
-    );
+      await comicrawl.run();
+
+      expect(mockLogger.logChapterRequest).toHaveBeenCalled();
+      expect(mockCrawler.extractTitle).toHaveBeenCalledWith(url);
+      expect(mockCrawler.extractChapters).toHaveBeenCalledWith(url);
+      expect(
+        mockProgressManager.createChapterPreparationBar
+      ).toHaveBeenCalledWith(title, chapters.length);
+      expect(mockProgressManager.advanceChapterPreparation).toHaveBeenCalled();
+      expect(mockProgressManager.completeChapterPreparation).toHaveBeenCalled();
+      expect(mockLogger.logDownloadStarted).toHaveBeenCalledWith(
+        title,
+        chapters.length
+      );
+      expect(mockCrawler.terminate).toHaveBeenCalledTimes(1);
+    });
 
     it.each([
       {
@@ -136,68 +141,63 @@ describe("Comicrawl", () => {
         ],
       },
     ])(
-      "should extract all chapters starting at $startingAt successfully",
-      async ({ url, title, chapters, startingAt }) => {
-        const logSpy = jest.spyOn(console, "log");
-        const extractedChapters = chapters.slice(startingAt - 1);
+      "should extract chapters successfully when downloadOption = 'Partial'",
+      async () => {
+        const url = "https://example.com";
+        const title = "First Comic";
+        const chapters = [
+          { title: "Chapter 1", url: "c1" },
+          { title: "Chapter 2", url: "c2" },
+          { title: "Chapter 3", url: "c3" },
+        ];
+
         mockPrompt.getUrl.mockResolvedValue(url);
         mockPrompt.getDownloadOption.mockResolvedValue("Partial");
-        mockPrompt.getChaptersStartingAt.mockResolvedValue(startingAt);
+        mockPrompt.getChaptersStartingAt.mockResolvedValue(2);
         mockCrawler.extractTitle.mockResolvedValue(title);
         mockCrawler.extractChapters.mockResolvedValue(chapters);
+        mockCrawler.extractImageLinks.mockResolvedValue([]);
 
         await comicrawl.run();
 
-        expect(mockCrawler.extractTitle).toHaveBeenCalled();
-        expect(mockCrawler.extractChapters).toHaveBeenCalled();
-        expect(logSpy).toHaveBeenNthCalledWith(1, title);
-        expect(logSpy).toHaveBeenNthCalledWith(2, extractedChapters);
-        expect(mockChromium.terminate).toHaveBeenCalledTimes(1);
+        expect(mockPrompt.getChaptersStartingAt).toHaveBeenCalledWith(chapters);
+        expect(mockLogger.logDownloadStarted).toHaveBeenCalledWith(title, 2);
+        expect(mockCrawler.terminate).toHaveBeenCalled();
       }
     );
 
-    it.each([
-      {
-        url: "https://example.com",
-        title: "Comic 1",
-        chapters: [
-          {
-            title: "Chapter 1",
-            url: "example.com/chapter-1",
-          },
-          {
-            title: "Chapter 2",
-            url: "example.com/chapter-2",
-          },
-          {
-            title: "Chapter 3",
-            url: "example.com/chapter-3",
-          },
-        ],
-        selected: ["Chapter 2"],
-      },
-    ])(
-      "should extract all selected chapters successfully",
-      async ({ url, title, chapters, selected }) => {
-        const logSpy = jest.spyOn(console, "log");
-        const extractedChapters = chapters.filter((chapter) =>
-          selected.includes(chapter.title)
-        );
-        mockPrompt.getUrl.mockResolvedValue(url);
-        mockPrompt.getDownloadOption.mockResolvedValue("Selective");
-        mockPrompt.getChaptersFromList.mockResolvedValue(selected);
-        mockCrawler.extractTitle.mockResolvedValue(title);
-        mockCrawler.extractChapters.mockResolvedValue(chapters);
+    it("should correctly extract chapters when downloadOption = 'Selective'", async () => {
+      const url = "https://test.com";
+      const title = "Comic 1";
+      const chapters = [
+        {
+          title: "Chapter 1",
+          url: "test.com/chapter-1",
+        },
+        {
+          title: "Chapter 2",
+          url: "test.com/chapter-2",
+        },
+        {
+          title: "Chapter 3",
+          url: "test.com/chapter-3",
+        },
+      ];
+      const selected = ["Chapter 2"];
 
-        await comicrawl.run();
+      mockPrompt.getUrl.mockResolvedValue(url);
+      mockPrompt.getDownloadOption.mockResolvedValue("Selective");
+      mockPrompt.getChaptersFromList.mockResolvedValue(selected);
+      mockCrawler.extractTitle.mockResolvedValue(title);
+      mockCrawler.extractChapters.mockResolvedValue(chapters);
+      mockCrawler.extractImageLinks.mockResolvedValue([]);
 
-        expect(mockCrawler.extractTitle).toHaveBeenCalled();
-        expect(mockCrawler.extractChapters).toHaveBeenCalled();
-        expect(logSpy).toHaveBeenNthCalledWith(1, title);
-        expect(logSpy).toHaveBeenNthCalledWith(2, extractedChapters);
-        expect(mockChromium.terminate).toHaveBeenCalledTimes(1);
-      }
-    );
+      await comicrawl.run();
+
+      expect(mockPrompt.getChaptersFromList).toHaveBeenCalledWith(chapters);
+      expect(mockLogger.logDownloadStarted).toHaveBeenCalledWith(title, 1);
+      expect(mockCrawler.terminate).toHaveBeenCalled();
+    });
 
     it("should exit early if graphic novel does not contain any chapters", async () => {
       const title = "Chapterless comic";
