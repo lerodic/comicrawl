538b7fbfe6bc6bb5bcc9a502d2346c41e41734eb
lerodic <daniel@rodev.net>
2025-10-15 14:59:20 +0200

    feat(app): Add listener for "SIGINT" event

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import "reflect-metadata";
 import ComicrawlFactory from "./core/factories/ComicrawlFactory";
 
 const comicrawl = ComicrawlFactory.create();
+
+process.on("SIGINT", async () => {
+  await comicrawl.closeBrowser();
+  process.exit(0);
+});
+
 comicrawl.run();
