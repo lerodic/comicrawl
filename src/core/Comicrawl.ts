bc93af2c35c534846f22fec431e17715062558ea
lerodic <daniel@rodev.net>
2025-10-17 22:39:17 +0200

    refactor(app): Remove unused code

--- src/core/Comicrawl.ts
+++ src/core/Comicrawl.ts
@@ -2,8 +2,6 @@ import { boundClass } from "autobind-decorator";
 import { inject, injectable } from "inversify";
 import TYPES from "../config/inversify/inversify.types";
 import CrawlerFactory from "./factories/CrawlerFactory";
-import Logger from "./io/Logger";
-import CrawlerInitializationFailed from "./errors/CrawlerInitializationFailed";
 import ErrorHandler from "./error/ErrorHandler";
 import DownloadService from "./download/DownloadService";
 import PreparationService from "./download/PreparationService";
@@ -13,8 +11,6 @@ import PreparationService from "./download/PreparationService";
 class Comicrawl {
   constructor(
     @inject(TYPES.CrawlerFactory) private crawlerFactory: CrawlerFactory,
-    @inject(TYPES.Logger) private logger: Logger,
-    @inject(TYPES.ProgressManager) private progress: ProgressManager
     @inject(TYPES.PreparationService) private preparation: PreparationService,
     @inject(TYPES.DownloadService) private download: DownloadService,
     @inject(TYPES.ErrorHandler) private errorHandler: ErrorHandler,
@@ -31,21 +27,6 @@ class Comicrawl {
       await this.closeBrowser();
     }
   }
-  private handleError(err: any) {
-    const message = this.isApplicationError(err)
-      ? err.message
-      : "Something unexpected happened.";
-
-    this.logger.error(`\n${message}`);
-  }
-
-  private isApplicationError(err: any): boolean {
-    return (
-      err instanceof EmptyGraphicNovel ||
-      err instanceof CrawlerInitializationFailed
-    );
-  }
-
   async closeBrowser() {
     await this.crawlerFactory.getCrawler().terminate();
   }
