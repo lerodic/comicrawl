20f4de820d9c4dc3d86c815c3d4f235c112c9258
lerodic <daniel@rodev.net>
2025-10-17 18:10:18 +0200

    feat(app): Extract dedicated "ErrorHandler" class

---
+++ src/core/error/ErrorHandler.ts
@@ -0,0 +1,30 @@
+import { boundClass } from "autobind-decorator";
+import { inject, injectable } from "inversify";
+import TYPES from "../../config/inversify/inversify.types";
+import Logger from "../io/Logger";
+import EmptyGraphicNovel from "./errors/EmptyGraphicNovel";
+import CrawlerInitializationFailed from "./errors/CrawlerInitializationFailed";
+
+@boundClass
+@injectable()
+class ErrorHandler {
+  constructor(@inject(TYPES.Logger) private logger: Logger) {}
+
+  handle(err: any) {
+    const message = this.isApplicationError(err)
+      ? err.message
+      : "Something unexpected happened.";
+
+    this.logger.error(`\n${message}`);
+  }
+
+  private isApplicationError(err: any): boolean {
+    const applicationErrors = [EmptyGraphicNovel, CrawlerInitializationFailed];
+
+    return applicationErrors.some(
+      (applicationError) => err instanceof applicationError
+    );
+  }
+}
+
+export default ErrorHandler;
