0bf0b4c8626565c666649bedf6041652b72a95f6
lerodic <daniel@rodev.net>
2025-10-17 21:20:15 +0200

    test(app): Update unit tests for "performance"

--- tests/performance.test.ts
+++ tests/performance.test.ts
@@ -1,86 +1,109 @@
-import { getConcurrencyLevel } from "../src/utils/performance";
+import { limit } from "../src/utils/performance";
 import os from "os";
+import pLimit from "p-limit";
+
+jest.mock("os");
+jest.mock("p-limit", () => {
+  return jest.fn((_: number) => {
+    const fn = (task: (...args: any[]) => any) => task();
+    return fn;
+  });
+});
 
 describe("performance", () => {
-  describe("getConcurrencyLevel", () => {
+  const mockOs = os as jest.Mocked<typeof os>;
+  const mockPLimit = pLimit as jest.MockedFunction<typeof pLimit>;
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("limit", () => {
     it.each([
       {
-        memoryInGb: 6,
-        expected: 1,
-      },
-      {
-        memoryInGb: 7,
-        expected: 1,
-      },
-      {
-        memoryInGb: 8,
-        expected: 2,
-      },
-      {
-        memoryInGb: 9,
-        expected: 2,
+        memoryInGb: 16,
+        concurrencyLevel: 5,
+        tasks: [
+          async () => 1,
+          async () => 2,
+          async () => 3,
+          async () => 4,
+          async () => 5,
+        ],
+        expected: [1, 2, 3, 4, 5],
       },
       {
         memoryInGb: 15,
-        expected: 2,
-      },
-      {
-        memoryInGb: 16,
-        expected: 5,
-      },
-      {
-        memoryInGb: 17,
-        expected: 5,
-      },
-      {
-        memoryInGb: 31,
-        expected: 5,
+        concurrencyLevel: 2,
+        tasks: [
+          async () => 1,
+          async () => 2,
+          async () => 3,
+          async () => 4,
+          async () => 5,
+        ],
+        expected: [1, 2, 3, 4, 5],
       },
       {
         memoryInGb: 32,
-        expected: 10,
-      },
-      {
-        memoryInGb: 33,
-        expected: 10,
-      },
-      {
-        memoryInGb: 63,
-        expected: 10,
+        concurrencyLevel: 10,
+        tasks: [
+          async () => 1,
+          async () => 2,
+          async () => 3,
+          async () => 4,
+          async () => 5,
+          async () => 6,
+          async () => 7,
+          async () => 8,
+          async () => 9,
+          async () => 10,
+        ],
+        expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       },
       {
         memoryInGb: 64,
-        expected: 15,
-      },
-      {
-        memoryInGb: 64,
-        expected: 15,
-      },
-      {
-        memoryInGb: 127,
-        expected: 15,
+        concurrencyLevel: 15,
+        tasks: [
+          async () => 1,
+          async () => 2,
+          async () => 3,
+          async () => 4,
+          async () => 5,
+          async () => 6,
+          async () => 7,
+          async () => 8,
+          async () => 9,
+          async () => 10,
+        ],
+        expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       },
       {
         memoryInGb: 128,
-        expected: 20,
-      },
-      {
-        memoryInGb: 129,
-        expected: 20,
-      },
-      {
-        memoryInGb: 256,
-        expected: 20,
+        concurrencyLevel: 20,
+        tasks: [
+          async () => 1,
+          async () => 2,
+          async () => 3,
+          async () => 4,
+          async () => 5,
+          async () => 6,
+          async () => 7,
+          async () => 8,
+          async () => 9,
+          async () => 10,
+        ],
+        expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       },
     ])(
-      "should return $expected for $memoryInGb GB RAM",
-      ({ memoryInGb, expected }) => {
-        const memory = memoryInGb * Math.pow(1024, 3);
-        jest.spyOn(os, "totalmem").mockReturnValue(memory);
+      "should limit execution to $concurrencyLevel for machine with $memoryInGb GB of RAM",
+      async ({ memoryInGb, concurrencyLevel, tasks, expected }) => {
+        mockOs.totalmem.mockReturnValue(memoryInGb * Math.pow(1024, 3));
 
-        const result = getConcurrencyLevel();
+        const result = await limit(tasks);
 
         expect(result).toStrictEqual(expected);
+        expect(mockPLimit).toHaveBeenCalledWith(concurrencyLevel);
       }
     );
   });
