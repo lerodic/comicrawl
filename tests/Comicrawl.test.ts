0c3ec53c0e3071347bfed89ae8e773671d36405f
lerodic <daniel@rodev.net>
2025-10-18 09:39:07 +0200

    test(app): Update unit tests for "Comicrawl"

--- tests/Comicrawl.test.ts
+++ tests/Comicrawl.test.ts
@@ -1,7 +1,6 @@
 import "reflect-metadata";
 import Comicrawl from "../src/core/Comicrawl";
-import { Crawler } from "../src/types";
-import CrawlerFactory from "../src/core/factories/CrawlerFactory";
+import { EventEmitter } from "../src/types";
 import CrawlerInitializationFailed from "../src/core/error/errors/CrawlerInitializationFailed";
 import PreparationService from "../src/core/download/PreparationService";
 import EmptyGraphicNovel from "../src/core/error/errors/EmptyGraphicNovel";
@@ -18,9 +17,8 @@ describe("Comicrawl", () => {
   let comicrawl: Comicrawl;
   let mockPreparationService: jest.Mocked<PreparationService>;
   let mockDownloadService: jest.Mocked<DownloadService>;
-  let mockCrawler: jest.Mocked<Crawler>;
-  let mockCrawlerFactory: jest.Mocked<CrawlerFactory>;
   let mockErrorHandler: jest.Mocked<ErrorHandler>;
+  let mockEventEmitter: jest.Mocked<EventEmitter>;
 
   beforeEach(() => {
     mockPreparationService = {
@@ -31,26 +29,20 @@ describe("Comicrawl", () => {
       start: jest.fn(),
     } as unknown as jest.Mocked<DownloadService>;
 
-    mockCrawler = {
-      extractTitle: jest.fn(),
-      extractChapters: jest.fn(),
-      extractImageLinks: jest.fn(),
-      terminate: jest.fn(),
-    } as unknown as jest.Mocked<Crawler>;
-
     mockErrorHandler = {
       handle: jest.fn(),
     } as unknown as jest.Mocked<ErrorHandler>;
 
-    mockCrawlerFactory = {
-      getCrawler: jest.fn().mockReturnValue(mockCrawler),
-    } as unknown as jest.Mocked<CrawlerFactory>;
+    mockEventEmitter = {
+      on: jest.fn(),
+      emit: jest.fn(),
+    } as unknown as jest.Mocked<EventEmitter>;
 
     comicrawl = new Comicrawl(
       mockPreparationService,
       mockDownloadService,
-      mockCrawlerFactory,
-      mockErrorHandler
+      mockErrorHandler,
+      mockEventEmitter
     );
   });
 
@@ -59,36 +51,36 @@ describe("Comicrawl", () => {
   });
 
   describe("run", () => {
-    describe("download whole comic", () => {
-      it.each([
-        {
-          title: "Comic 1",
-          chapters: [
-            {
-              title: "Chapter 1",
-              url: "example.com/chapter-1",
-              imageLinks: ["img1", "img2", "img3"],
-            },
-            {
-              title: "Chapter 2",
-              url: "example.com/chapter-2",
-              imageLinks: ["img1", "img2", "img3", "img4"],
-            },
-            {
-              title: "Chapter 3",
-              url: "example.com/chapter-3",
-              imageLinks: ["img1", "img2", "img3"],
-            },
-          ],
-        },
-      ])("should execute correctly", async ({ title, chapters }) => {
-        mockPreparationService.start.mockResolvedValue({ title, chapters });
+    it.each([
+      {
+        title: "Comic 1",
+        chapters: [
+          {
+            title: "Chapter 1",
+            url: "example.com/chapter-1",
+            imageLinks: ["img1", "img2", "img3"],
+          },
+          {
+            title: "Chapter 2",
+            url: "example.com/chapter-2",
+            imageLinks: ["img1", "img2", "img3", "img4"],
+          },
+          {
+            title: "Chapter 3",
+            url: "example.com/chapter-3",
+            imageLinks: ["img1", "img2", "img3"],
+          },
+        ],
+      },
+    ])("should execute correctly", async ({ title, chapters }) => {
+      mockPreparationService.start.mockResolvedValue({ title, chapters });
 
-        await comicrawl.run();
+      await comicrawl.run();
 
-        expect(mockDownloadService.start).toHaveBeenCalledWith(title, chapters);
-        expect(mockCrawler.terminate).toHaveBeenCalledTimes(1);
-      });
+      expect(mockDownloadService.start).toHaveBeenCalledWith(title, chapters);
+      expect(mockEventEmitter.emit).toHaveBeenCalledWith(
+        "applicationTerminated"
+      );
     });
 
     it("should exit early if graphic novel does not contain any chapters", async () => {
@@ -100,7 +92,9 @@ describe("Comicrawl", () => {
       await comicrawl.run();
 
       expect(mockErrorHandler.handle).toHaveBeenCalledWith(error);
-      expect(mockCrawler.terminate).toHaveBeenCalledTimes(1);
+      expect(mockEventEmitter.emit).toHaveBeenCalledWith(
+        "applicationTerminated"
+      );
     });
 
     it("should exit early if crawler initialization fails", async () => {
@@ -112,7 +106,9 @@ describe("Comicrawl", () => {
       await comicrawl.run();
 
       expect(mockErrorHandler.handle).toHaveBeenCalledWith(error);
-      expect(mockCrawler.terminate).toHaveBeenCalled();
+      expect(mockEventEmitter.emit).toHaveBeenCalledWith(
+        "applicationTerminated"
+      );
     });
 
     it("should terminate correctly on unexpected error", async () => {
@@ -124,7 +120,9 @@ describe("Comicrawl", () => {
       await comicrawl.run();
 
       expect(mockErrorHandler.handle).toHaveBeenCalledWith(error);
-      expect(mockCrawler.terminate).toHaveBeenCalledTimes(1);
+      expect(mockEventEmitter.emit).toHaveBeenCalledWith(
+        "applicationTerminated"
+      );
     });
   });
 });
