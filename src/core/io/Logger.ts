9d3e0ed65df8157fe4447aabb4837ffd7f6981e7
lerodic <daniel@rodev.net>
2025-10-27 11:43:32 +0100

    feat(app): Add method to "Logger" to tell user about log file contents

--- src/core/io/Logger.ts
+++ src/core/io/Logger.ts
@@ -1,6 +1,7 @@
 import { boundClass } from "autobind-decorator";
 import { injectable } from "inversify";
 import chalk from "chalk";
+import { SourceOfTermination } from "../../types";
 
 @boundClass
 @injectable()
@@ -13,8 +14,12 @@ class Logger {
     console.log(chalk.redBright.bold(message));
   }
 
-  logChapterRequest() {
-    this.info("\nRequesting chapter list. This may take a moment.\n");
+  warn(message: string) {
+    console.log(chalk.yellowBright.bold(message));
+  }
+
+  success(message: string) {
+    console.log(chalk.greenBright.bold(message));
   }
 
   logChaptersFound(title: string, chapterCounter: number) {
@@ -23,6 +28,54 @@ class Logger {
         chapterCounter
       )} chapters for '${chalk.blueBright.bold(title)}'.\n`
     );
+  }
+
+  logSessionOutcome(
+    sourceOfTermination: SourceOfTermination,
+    numFailedDownloads: number
+  ) {
+    const method = this.getSessionOutcomeMethod(
+      sourceOfTermination,
+      numFailedDownloads
+    );
+    const message = this.getSessionOutcomeMessage(
+      sourceOfTermination,
+      numFailedDownloads
+    );
+
+    method(`\n${message}`);
+  }
+
+  private getSessionOutcomeMethod(
+    sourceOfTermination: SourceOfTermination,
+    numFailedDownloads: number
+  ) {
+    switch (sourceOfTermination) {
+      case "Error":
+        return this.error;
+      case "Program":
+        return numFailedDownloads === 0 ? this.success : this.warn;
+      case "User":
+        return this.info;
+    }
+  }
+
+  private getSessionOutcomeMessage(
+    sourceOfTermination: SourceOfTermination,
+    numFailedDownloads: number
+  ): string {
+    switch (sourceOfTermination) {
+      case "User":
+        return "Application exited by user.";
+      case "Error":
+        return "The application encountered an error. Check the log file for more info.";
+      case "Program":
+        return numFailedDownloads === 0
+          ? "Download completed. No errors have been tracked."
+          : `${chalk.whiteBright.bold(numFailedDownloads)} ${
+              numFailedDownloads === 1 ? "download" : "downloads"
+            } failed. Check the log file for more info.`;
+    }
   }
 }
 
