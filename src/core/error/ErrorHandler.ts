1cad1b8d222d8eed40bd11d9fc8c396c3816a818
lerodic <daniel@rodev.net>
2025-11-11 14:32:17 +0100

    feat(app): Properly handle network-related errors in "ErrorHandler"

--- src/core/error/ErrorHandler.ts
+++ src/core/error/ErrorHandler.ts
@@ -5,6 +5,7 @@ import Logger from "../io/Logger";
 import EmptyGraphicNovel from "./errors/EmptyGraphicNovel";
 import CrawlerInitializationFailed from "./errors/CrawlerInitializationFailed";
 import LogFileCreationFailed from "./errors/LogFileCreationFailed";
+import ConnectionInterrupted from "./errors/ConnectionInterrupted";
 
 @boundClass
 @injectable()
@@ -12,11 +13,31 @@ class ErrorHandler {
   constructor(@inject(TYPES.Logger) private logger: Logger) {}
 
   handle(err: any) {
-    const message = this.isApplicationError(err)
-      ? err.message
-      : "Something unexpected happened.";
+    const message = this.getCorrectErrorMessage(err);
 
     this.logger.error(`\n${message}`);
+  }
+
+  private getCorrectErrorMessage(err: any): string {
+    if (this.isNetworkError(err)) {
+      return "Network connection lost.";
+    } else if (this.isApplicationError(err)) {
+      return err.message;
+    } else {
+      return "Something unexpected happened.";
+    }
+  }
+
+  private isNetworkError(err: any): boolean {
+    const networkRelatedMessages = [
+      "getaddrinfo ENOTFOUND",
+      "net::ERR_INTERNET_DISCONNECTED",
+    ];
+
+    return (
+      err instanceof ConnectionInterrupted ||
+      networkRelatedMessages.some((message) => err.message.includes(message))
+    );
   }
 
   private isApplicationError(err: any): boolean {
