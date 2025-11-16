832d8b9a9ef6621ac37c93011f94f3dfe666178e
lerodic <daniel@rodev.net>
2025-11-16 10:10:28 +0100

    test(app): Update unit tests for 'Prompt'

--- tests/Prompt.test.ts
+++ tests/Prompt.test.ts
@@ -14,7 +14,7 @@ jest.mock("inquirer");
 jest.mock("../src/config/app.config", () => ({
   __esModule: true,
   default: {
-    SUPPORTED_DOMAINS: ["https://mocked.com", "https://mocked2.com"],
+    SUPPORTED_ORIGINS: ["https://mocked.com", "https://mocked2.com"],
     EXECUTABLE_PATH: "/mocked/path",
   },
 }));
@@ -36,7 +36,7 @@ describe("Prompt", () => {
   });
 
   describe("getUrl", () => {
-    it.each(mockedConfig.SUPPORTED_DOMAINS)(
+    it.each(mockedConfig.SUPPORTED_ORIGINS)(
       "should return '%s' if user enters a valid URL",
       async (url) => {
         mockInquirer.prompt.mockResolvedValue({ url });
@@ -62,7 +62,7 @@ describe("Prompt", () => {
           "\nPlease enter a valid URL. Below is a list of valid domains:\n"
         );
         expect(mockLogger.info).toHaveBeenCalledTimes(
-          mockedConfig.SUPPORTED_DOMAINS.length
+          mockedConfig.SUPPORTED_ORIGINS.length
         );
         expect(result).toStrictEqual("mocked-second-call");
         expect(getUrlSpy).toHaveBeenCalledTimes(2);
